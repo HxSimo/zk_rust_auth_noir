de4fae880bcdd662e7dfe9f5e88104e1dcdea066
HxSimo <HxSimo@proton.me>
2025-03-27 23:11:58 -0300

    Scalar mul in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use ark_bn254::{Fr, G1Projective};
+use ark_ec::{CurveGroup, PrimeGroup};
+use std::str::FromStr;
+use std::ops::Mul;
+
 fn main() {
-    println!("Hello, world!");
+    let sk = Fr::from_str("12345").unwrap();
+    let g = G1Projective::generator();    // generate G
+    let pubkey = g.mul(sk);     // Scalar mul : G * sk
+    let affine = pubkey.into_affine();
+
+    println!("x = {}", affine.x);
+    println!("y = {}", affine.y);
 }
